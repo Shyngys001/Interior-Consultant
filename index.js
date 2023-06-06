43c7d013ad5e0e7a711ce4ee5097fadfb591272c
Shyngys <93879495+Shyngys001@users.noreply.github.com>
2023-06-06 20:59:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-document.addEventListener('DOMContentLoaded', nav)
-function nav(){
-    const burger = document.querySelector('.burger');
+document.addEventListener('DOMContentLoaded', () => {
     const nav = document.querySelector('.main-nav');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
-}
+
+    const burger = nav.querySelector('.burger');
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  });
+  
+  let isHidden = false;
+
+  function toggleVisibility() {
+    const elementsToShowHide = document.querySelectorAll('.main, footer');
+  
+    elementsToShowHide.forEach(element => {
+      element.style.display = isHidden ? 'block' : 'none';
+    });
+  
+    isHidden = !isHidden;
+  }
+  
+  function hide() {
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', toggleVisibility);
+  }
+  
